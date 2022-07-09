c70173f70faa4464e3dfe224176856331cbf2366
gaharavara <abhishek.singh@nova-benefits.com>
2022-07-10 03:49:28 +0530

    Modify sample-script to deploye Dsocial.sol

--- backend/scripts/sample-script.js
+++ backend/scripts/sample-script.js
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const Dsocial = await hre.ethers.getContractFactory("Dsocial");
+  const dSocial = await Dsocial.deploy();
 
-  await greeter.deployed();
+  await dSocial.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("DSocial deployed to:", dSocial.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
